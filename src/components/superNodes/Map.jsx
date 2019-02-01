bfd67bbd3a3c9eca86b117f00aa10bcb2072026a
hofungkoeng <hefq@yunphant.com>
2019-02-01 14:22:40 +0800

    add map draft

--- src/components/superNodes/Map.jsx
+++ src/components/superNodes/Map.jsx
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
-export default function Map() {
-  return <div>map</div>
+import 'leaflet/dist/leaflet.css'
+
+export default class SimpleExample extends Component {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng]
+    return (
+      <div>
+        <Map
+          style={{ height: '600px', width: '1000px' }}
+          center={position}
+          zoom={this.state.zoom}
+        >
+          <TileLayer
+            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </Map>
+      </div>
+    )
+  }
 }
