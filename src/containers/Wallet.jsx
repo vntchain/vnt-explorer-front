ce45c21030d32d41fd03f538d3a067dbadca04c1
hofungkoeng <hefq@yunphant.com>
2019-04-29 10:32:09 +0800

    rewrite Wallet using class style

--- src/containers/Wallet.jsx
+++ src/containers/Wallet.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 import { Button, Spin } from 'antd'
 import { push } from 'react-router-redux'
@@ -9,6 +9,8 @@ import TabTxList from 'components/accountDetail/TabTxList'
 import TabTokenTxList from 'components/accountDetail/TabTokenTxList'
 import TabTokenBalanceList from 'components/accountDetail/TabTokenBalanceList'
 import BriefTable from 'components/BaseTable'
+import Copier from 'components/Copier'
+import QRModal from 'components/QRModal'
 
 import LocalText from 'i18n/LocalText'
 import DataProviderNew from 'containers/RPDataProviderNew'
@@ -31,191 +33,211 @@ const mapStateToProps = ({
   }
 }
 
-export default connect(mapStateToProps)(function WalletAccount(props) {
-  const { accountDetail } = props
-
-  // 点击发送方、接收方链接时更新表单数据
-  useEffect(() => {
-    props.dispatch({
-      type: 'dataRelayNew/fetchData',
-      payload: {
-        // path: `${apis.accountDetail}/${props.match.params.acct}`,
-        path: `${apis.accountDetail}/${props.account.address}`,
-        ns: 'accounts',
-        field: 'accountDetail'
-      }
-    })
-  }, [])
-
-  const columns = [
-    {
-      title: <LocalText id="waField1" />,
-      dataIndex: 'fieldName',
-      key: 'fieldName'
-    },
-    {
-      title:
-        props.accountDetail &&
-        props.accountDetail.data &&
-        props.accountDetail.data.hasOwnProperty('Balance')
-          ? `${props.accountDetail.data.Balance} VNT`
-          : '-/-',
-      dataIndex: 'value',
-      key: 'value'
+export default connect(mapStateToProps)(
+  class WalletAccount extends React.Component {
+    constructor(props) {
+      super(props)
+      this.copyRef = React.createRef()
     }
-  ]
-
-  const data = [
-    {
-      key: 'address',
-      fieldName: <LocalText id="waField2" />,
-      value: props.account.address
-    },
-    {
-      key: 'send-receive',
-      fieldName: <LocalText id="waField3" />,
-      value: (
-        <div>
-          <Button
-            className={styles.btn}
-            style={{ backgroundColor: '#4cc159', borderColor: '#4cc159' }}
-            type="primary"
-            onClick={() => props.dispatch(push(r.send))}
-          >
-            <LocalText id="waBtn1" />
-          </Button>
-          <Button
-            className={styles.btn}
-            type="primary"
-            onClick={() => props.dispatch(push(r.receive))}
-          >
-            <LocalText id="waBtn2" />
-          </Button>
-        </div>
-      )
+    componentDidMount() {
+      this.props.dispatch({
+        type: 'dataRelayNew/fetchData',
+        payload: {
+          // path: `${apis.accountDetail}/${props.match.params.acct}`,
+          path: `${apis.accountDetail}/${this.props.account.address}`,
+          ns: 'accounts',
+          field: 'accountDetail'
+        }
+      })
     }
-  ]
 
-  // const testAccount = '0x122369f04f32269598789998de33e3d56e2c507a'
-  const tabs = [
-    {
-      btnName: <LocalText id="adpField4" />,
-      comp: (
-        <Fragment key="1">
-          {/* 获取当前账户第一分页的交易 */}
-          <DataProviderNew
-            options={{
-              path:
-                `${apis.txs}?limit=${pageSize}&offset=0&account=` +
-                props.account.address,
-              ns: 'transactions',
-              field: 'filteredTxs'
-            }}
-            render={data => (
-              <PageProvider
-                comp={TabTxList}
+    render() {
+      const { accountDetail } = this.props
+      const columns = [
+        {
+          title: <LocalText id="waField1" />,
+          dataIndex: 'fieldName',
+          key: 'fieldName'
+        },
+        {
+          title:
+            this.props.accountDetail &&
+            this.props.accountDetail.data &&
+            this.props.accountDetail.data.hasOwnProperty('Balance')
+              ? `${this.props.accountDetail.data.Balance} VNT`
+              : '-/-',
+          dataIndex: 'value',
+          key: 'value'
+        }
+      ]
+
+      const data = [
+        {
+          key: 'address',
+          fieldName: <LocalText id="waField2" />,
+          value: (
+            <div className={styles.addr}>
+              <Copier
+                text={this.props.account.address}
+                style={''}
+                copyRef={this.copyRef}
+              />
+              <QRModal address={this.props.account.address} />
+            </div>
+          )
+        },
+        {
+          key: 'send-receive',
+          fieldName: <LocalText id="waField3" />,
+          value: (
+            <div>
+              <Button
+                className={styles.btn}
+                style={{ backgroundColor: '#4cc159', borderColor: '#4cc159' }}
+                type="primary"
+                onClick={() => this.props.dispatch(push(r.send))}
+              >
+                <LocalText id="waBtn1" />
+              </Button>
+              <Button
+                className={styles.btn}
+                type="primary"
+                onClick={() => this.props.dispatch(push(r.receive))}
+              >
+                <LocalText id="waBtn2" />
+              </Button>
+            </div>
+          )
+        }
+      ]
+
+      // const testAccount = '0x122369f04f32269598789998de33e3d56e2c507a'
+      const tabs = [
+        {
+          btnName: <LocalText id="adpField4" />,
+          comp: (
+            <Fragment key="1">
+              {/* 获取当前账户第一分页的交易 */}
+              <DataProviderNew
                 options={{
-                  basePath:
-                    `${apis.txs}?limit=${pageSize}&account=` +
-                    props.account.address,
+                  path:
+                    `${apis.txs}?limit=${pageSize}&offset=0&account=` +
+                    this.props.account.address,
                   ns: 'transactions',
                   field: 'filteredTxs'
                 }}
-                refreshProof={false}
-                context={data}
-                currentIndex={1}
+                render={data => (
+                  <PageProvider
+                    comp={TabTxList}
+                    options={{
+                      basePath:
+                        `${apis.txs}?limit=${pageSize}&account=` +
+                        this.props.account.address,
+                      ns: 'transactions',
+                      field: 'filteredTxs'
+                    }}
+                    refreshProof={false}
+                    context={data}
+                    currentIndex={1}
+                  />
+                )}
               />
-            )}
-          />
-        </Fragment>
-      )
-    },
-    {
-      btnName: <LocalText id="adpField5" />,
-      comp: (
-        <Fragment key="2">
-          {/* 获取当前账户第一分页的代币交易 */}
-          <DataProviderNew
-            options={{
-              path:
-                `${apis.txs}?isToken=1&limit=${pageSize}&offset=0&account=` +
-                props.account.address,
-              ns: 'transactions',
-              field: 'filteredTxs'
-            }}
-            render={data => (
-              <PageProvider
-                comp={TabTokenTxList}
+            </Fragment>
+          )
+        },
+        {
+          btnName: <LocalText id="adpField5" />,
+          comp: (
+            <Fragment key="2">
+              {/* 获取当前账户第一分页的代币交易 */}
+              <DataProviderNew
                 options={{
-                  basePath:
-                    `${apis.txs}?isToken=1&limit=${pageSize}&account=` +
-                    props.account.address,
+                  path:
+                    `${
+                      apis.txs
+                    }?isToken=1&limit=${pageSize}&offset=0&account=` +
+                    this.props.account.address,
                   ns: 'transactions',
                   field: 'filteredTxs'
                 }}
-                refreshProof={false}
-                context={data}
-                currentIndex={1}
+                render={data => (
+                  <PageProvider
+                    comp={TabTokenTxList}
+                    options={{
+                      basePath:
+                        `${apis.txs}?isToken=1&limit=${pageSize}&account=` +
+                        this.props.account.address,
+                      ns: 'transactions',
+                      field: 'filteredTxs'
+                    }}
+                    refreshProof={false}
+                    context={data}
+                    currentIndex={1}
+                  />
+                )}
               />
-            )}
-          />
-        </Fragment>
-      )
-    },
-    {
-      btnName: <LocalText id="adpField6" />,
-      comp: (
-        <Fragment key="3">
-          {/* 获取当前账户第一分页的代币 */}
-          <DataProviderNew
-            options={{
-              path: `${apis.accountDetail}/${
-                props.account.address
-              }/tokens?limit=${pageSize}&offset=0`,
-              ns: 'accounts',
-              field: 'tokens'
-            }}
-            render={data => (
-              <PageProvider
-                comp={TabTokenBalanceList}
+            </Fragment>
+          )
+        },
+        {
+          btnName: <LocalText id="adpField6" />,
+          comp: (
+            <Fragment key="3">
+              {/* 获取当前账户第一分页的代币 */}
+              <DataProviderNew
                 options={{
-                  basePath: `${apis.accountDetail}/${
-                    props.account.address
-                  }/tokens?limit=${pageSize}`,
+                  path: `${apis.accountDetail}/${
+                    this.props.account.address
+                  }/tokens?limit=${pageSize}&offset=0`,
                   ns: 'accounts',
                   field: 'tokens'
                 }}
-                refreshProof={false}
-                context={data}
-                currentIndex={1}
+                render={data => (
+                  <PageProvider
+                    comp={TabTokenBalanceList}
+                    options={{
+                      basePath: `${apis.accountDetail}/${
+                        this.props.account.address
+                      }/tokens?limit=${pageSize}`,
+                      ns: 'accounts',
+                      field: 'tokens'
+                    }}
+                    refreshProof={false}
+                    context={data}
+                    currentIndex={1}
+                  />
+                )}
               />
-            )}
-          />
-        </Fragment>
+            </Fragment>
+          )
+        }
+      ]
+      return (
+        <div>
+          <PrimaryTitle id="waTitle" />
+
+          <Spin spinning={accountDetail && accountDetail.isLoading}>
+            <div className={styles.tabTableContainer}>
+              <BriefTable
+                columns={columns}
+                data={data}
+                tableType="2colDetail"
+                pagination={false}
+              />
+
+              <Margin size="medium" />
+              {accountDetail &&
+                !accountDetail.isLoading && (
+                  <Tabs
+                    key={Date.now()}
+                    tabs={tabs}
+                    dispatch={this.props.dispatch}
+                  />
+                )}
+            </div>
+          </Spin>
+        </div>
       )
     }
-  ]
-
-  return (
-    <div>
-      <PrimaryTitle id="waTitle" />
-
-      <Spin spinning={accountDetail && accountDetail.isLoading}>
-        <div className={styles.tabTableContainer}>
-          <BriefTable
-            columns={columns}
-            data={data}
-            tableType="2colDetail"
-            pagination={false}
-          />
-
-          <Margin size="medium" />
-          {accountDetail &&
-            !accountDetail.isLoading && (
-              <Tabs key={Date.now()} tabs={tabs} dispatch={props.dispatch} />
-            )}
-        </div>
-      </Spin>
-    </div>
-  )
-})
+  }
+)
